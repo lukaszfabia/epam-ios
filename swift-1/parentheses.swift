31e3957d7eff26d981b77d04ad7ca9fd73030c5d
Lukasz <ufabia03@gmail.com>
2025-05-21 12:05:12 +0200

    fix: optimized and improved func

--- swift-1/parentheses.swift
+++ swift-1/parentheses.swift
@@ -2,55 +2,70 @@
 // - The input string might contain multiple lines.
 // - For example, "(())" is balanced, but "(()" is not.
 
-
-// (()) -> balanced
-// ()()
-// (())(((())))
-// )( -> nie jest valid
-
 public func isBalancedParentheses(input: String) -> Bool {
-    // Only parentheses ()
-    let cleanInput = input.filter { c in
-        return c == "(" || c == ")"
-    }
-
-    // every '(' must be close by ')', len of str must be even
-    if cleanInput.count % 2 != 0 {
-        return false
-    }
-
     var stack: [String.Element] = []
 
-    for char in cleanInput  {
-        // we track only opening
-        if char == "(" {
-            stack.append(char)
-        } 
-        // if is closing
-        // then we should have last opening
-        else if stack.last == "(" {
-            _=stack.popLast()
-        }
-        // [(,(] ->
-        else {
-            return false
+    for char in input  {
+        // live filtering
+        if char == ")" || char == "(" {
+            if char == "(" {
+                stack.append(char)
+            } 
+            // if is closing
+            // then we should have last opening
+            else if stack.last == "(" {
+                _=stack.popLast()
+            }
+
+            else {
+                return false
+            }
         }
     }
     
-    return stack.count == 0 // stack must be empty
+    return stack.isEmpty
 }
 
+assert(!isBalancedParentheses(input: ")()("))
 assert(!isBalancedParentheses(input: "(()"))
 assert(isBalancedParentheses(input: "()()"))
+assert(isBalancedParentheses(input: "(()())"))
 assert(!isBalancedParentheses(input: "())("))
 assert(!isBalancedParentheses(input: ")("))
 assert(!isBalancedParentheses(input: "2+2*(5+2))"))
 assert(isBalancedParentheses(input: "((()))"))
 assert(!isBalancedParentheses(input: "((()))))"))
+assert(isBalancedParentheses(input: ""))
+assert(!isBalancedParentheses(input: "(d)asa)"))
+assert(!isBalancedParentheses(input: ")()("))
+assert(!isBalancedParentheses(input: "(((as)asa(sas)asa(sa"))
+
 
 let bigstr = """
 (((())))(((()))))))())(
 """
 assert(!isBalancedParentheses(input: bigstr))
+
+let bigstr1 = """
+(
+    (
+        (
+        ()()(())
+        )
+    )
+)
+""" 
+assert(isBalancedParentheses(input: bigstr1))
+
+let weirdStr = """
+(
+)
+(
+)
+(
+)
+"""
+assert(isBalancedParentheses(input: weirdStr))
+
 
 
