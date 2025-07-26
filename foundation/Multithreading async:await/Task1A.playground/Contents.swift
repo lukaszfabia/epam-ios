0df9b4fb8c014d874aa50936728db8b0a675c058
Lukasz <ufabia03@gmail.com>
2025-07-26 18:23:21 +0200

    add: task1

---
+++ foundation/Multithreading async:await/Task1A.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+let mainQueue = OperationQueue.main
+let queue = OperationQueue()
+// order guarantee
+queue.maxConcurrentOperationCount = 2
+
+let operation = BlockOperation {
+    print(Thread.current)
+    print("Operation \"A\" started")
+    for _ in 0..<10 {
+        // do nothing
+        sleep(1)
+    }
+    print("Operation \"A\" finished")
+}
+
+let other = BlockOperation {
+    print(Thread.current)
+    
+    sleep(3)
+    
+    print("end")
+}
+
+mainQueue.addOperation(operation)
+mainQueue.addOperation {
+    print("needed to wait ;/")
+    sleep(1)
+}
+queue.addOperation(other)
+queue.addOperation {
+    print(Thread.current)
+    sleep(1)
+    print("one more other done")
+}
+queue.addOperation {
+    print(Thread.current)
+    print("no time operation will be executed as a last one in this queue")
+}
